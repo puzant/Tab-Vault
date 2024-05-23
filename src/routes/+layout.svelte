ee5e2d788d577f36be00fcaa4abd8db7986191c0
Puzant <pbakjejian@hotmail.com>
2024-05-23 12:04:51 +0300

    seveltekit setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/settings">Settings</a>
+</nav>
+
+<slot></slot>
+
+<script>
+  import "../app.css";
+</script>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<h1>Your Library</h1>
+
+<div class="flex flex-col gap-2 my-4">
+  <div class="flex gap-1">
+    <img src="/images/plus.svg" alt="">
+    <button>Link new tab</button>
+  </div>
+  
+  <div class="flex gap-1">
+    <img src="/images/plus.svg" alt="">
+    <button>Upload PDF Tab</button>
+  </div>
+  
+  <div class="flex gap-1">
+    <img src="/images/plus.svg" alt="">
+    <button>Upload Image Tab</button>
+  </div>
+</div>
